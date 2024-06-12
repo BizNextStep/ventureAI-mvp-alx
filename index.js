88571a2749f6d91abebce20cc13ae61a4058b460
BizNextStep <121868497+BizNextStep@users.noreply.github.com>
2024-06-12 21:06:45 +0200

    Update index.js
    Render React application to the HTML element with the id "root"

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+# Import necessary libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+# Render the React application
 ReactDOM.render(
+  // Wrap the application with the React.StrictMode component
   <React.StrictMode>
+    // Render the App component
     <App />
   </React.StrictMode>,
+  // Render the application to the HTML element with the id "root"
   document.getElementById('root')
 );
